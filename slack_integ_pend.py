c21e1695530e240c7ee75bfe576a629aaad9e38c
Revanth.Bobba <41135005+revanth543@users.noreply.github.com>
2019-11-20 13:51:15 +0530

    Add files via upload

---
+++ slack_integ_pend.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# Copyright (c) 2019 @Revanth
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+import re
+
+import certstream
+import entropy
+import tqdm
+import yaml
+import time
+import os
+from Levenshtein import distance
+from termcolor import colored, cprint
+from tld import get_tld
+from slackclient import SlackClient
+
+from confusables import unconfuse
+
+certstream_url = 'wss://certstream.calidog.io'
+
+#Below Target List you can give n no.of comany names
+
+target=["abc", "XYZ"] 
+
+
+log_suspicious = os.path.dirname(os.path.realpath(__file__))+'/suspicious_domains_'+time.strftime("%Y-%m-%d")+'.log'
+
+suspicious_yaml = os.path.dirname(os.path.realpath(__file__))+'/suspicious.yaml'
+
+external_yaml = os.path.dirname(os.path.realpath(__file__))+'/external.yaml'
+
+pbar = tqdm.tqdm(desc='certificate_update', unit='cert')
+
+def score_domain(domain):
+ 
+ """Score `domain`. The highest score, the most probable `domain` is a phishing site.
+    Args:
+        domain (str): the domain to check.
+    Returns:
+        int: the score of `domain`.
+ """
+    score = 0
+    for t in suspicious['tlds']:
+        if domain.endswith(t):
+            score += 20
+
+    # Remove initial '*.' for wildcard certificates bug
+    if domain.startswith('*.'):
+        domain = domain[2:]
+
+    # Removing TLD to catch inner TLD in subdomain (ie. xyz.com.domain.com)
+    try:
+        res = get_tld(domain, as_object=True, fail_silently=True, fix_protocol=True)
+        domain = '.'.join([res.subdomain, res.domain])
+    except Exception:
+        pass
+
+    # Higher entropy is kind of suspicious
+    score += int(round(entropy.shannon_entropy(domain)*50))
+
+    # Remove look alike characters using list from http://www.unicode.org/reports/tr39
+    domain = unconfuse(domain)
+
+    words_in_domain = re.split("\W+", domain)
+
+    # ie. detect fake.com (ie. *.com-account-management.info)
+    if words_in_domain[0] in ['com', 'net', 'org']:
+        score += 10
+
+    # Testing keywords
+    for word in suspicious['keywords']:
+        if word in domain:
+            score += suspicious['keywords'][word]
+
+    # Testing Levenshtein distance for strong keywords (>= 70 points) (ie. paypol)
+    for key in [k for (k,s) in suspicious['keywords'].items() if s >= 70]:
+        # Removing too generic keywords (ie. mail.domain.com)
+        for word in [w for w in words_in_domain if w not in ['email', 'mail', 'cloud']]:
+            if distance(str(word), str(key)) == 1:
+                score += 70
+
+    # Lots of '-' (ie. www.xyz-datacenter.com-acccount-alert.com)
+    if 'xn--' not in domain and domain.count('-') >= 4:
+        score += domain.count('-') * 3
+
+    # Deeply nested subdomains (ie. www.xyz.com.security.accountupdate.gq)
+    if domain.count('.') >= 3:
+        score += domain.count('.') * 3
+
+    return score
+
+""" Slack Integration for Alerting """
+def slack_message(msg):
+    token = 'xoxp-82XXXXXX-XXXXXXXXXXX-XXXXXXXXXXXXXXXXX-XXXXXXXXXXXXXXXxx'
+    sc = SlackClient(token)
+    sc.api_call('chat.postMessage', channel='secops',
+                text=msg, username='revanth.bobba',
+                icon_emoji=':robot_face:')
+
+def callback(message, context):
+    """Callback handler for certstream events."""
+    if message['message_type'] == "heartbeat":
+        return
+
+    if message['message_type'] == "certificate_update":
+        all_domains = message['data']['leaf_cert']['all_domains']
+
+        for domain in all_domains:
+            pbar.update(1)
+			for targets in target:
+				if re.search(targets,domain):
+					score = score_domain(domain.lower())
+				# If issued from a free CA = more suspicious
+					if "Let's Encrypt" in message['data']['chain'][0]['subject']['aggregated']:
+						score += 10
+					if score >= 100:
+						tqdm.tqdm.write(
+							"[!] Suspicious: "
+							"{} (score={})".format(colored(domain, 'red', attrs=['underline', 'bold']), score))
+					elif score >= 90:
+						tqdm.tqdm.write(
+							"[!] Suspicious: "
+							"{} (score={})".format(colored(domain, 'red', attrs=['underline']), score))
+					elif score >= 80:
+						tqdm.tqdm.write(
+							"[!] Likely    : "
+							"{} (score={})".format(colored(domain, 'yellow', attrs=['underline']), score))
+					elif score >= 65:
+						tqdm.tqdm.write(
+							"[+] Potential : "
+							"{} (score={})".format(colored(domain, attrs=['underline']), score))
+					if score >= 85:
+						if any(x in domain for x in target):
+							slack_message(domain + score)
+						else:
+							print("[+] No alert.")
+						with open(log_suspicious, 'a') as f:
+							f.write("{} - (score={})\n".format(domain, score))
+if __name__ == '__main__':
+    with open(suspicious_yaml, 'r') as f:
+        suspicious = yaml.safe_load(f)
+
+    with open(external_yaml, 'r') as f:
+        external = yaml.safe_load(f)
+
+    if external['override_suspicious.yaml'] is True:
+        suspicious = external
+    else:
+        if external['keywords'] is not None:
+            suspicious['keywords'].update(external['keywords'])
+
+        if external['tlds'] is not None:
+            suspicious['tlds'].update(external['tlds'])
+
+    certstream.listen_for_events(callback, url=certstream_url)
